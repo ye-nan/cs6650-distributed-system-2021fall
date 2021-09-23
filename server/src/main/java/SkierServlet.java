1eff1941a3f27bbf2855f4b693794e7bb88b4b21
Nan Ye <ye.na@northeastern.edu>
2021-09-22 22:07:48 -0700

    Successfully start Tomcat server

---
+++ server/src/main/java/SkierServlet.java
@@ -0,0 +1,45 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "SkierServlet", value = "/SkierServlet")
+public class SkierServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        String urlPath = request.getPathInfo();
+
+        // check we have a URL!
+        if (urlPath == null || urlPath.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.getWriter().write("missing paramterers");
+            return;
+        }
+
+        String[] urlParts = urlPath.split("/");
+        // and now validate url path and return the response status code
+        // (and maybe also some value if input is valid)
+
+        if (!isUrlValid(urlParts)) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+            // do any sophisticated processing with urlParts which contains all the url params
+            // TODO: process url params in `urlParts`
+            response.getWriter().write("It works!");
+        }
+    }
+
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+
+    private boolean isUrlValid(String[] urlPath) {
+        // TODO: validate the request url path according to the API spec
+        // urlPath  = "/1/seasons/2019/day/1/skier/123"
+        // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
+        return true;
+    }
+}
