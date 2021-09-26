ea5c0856cd034646896fedc6e0b265d8735640df
Nan Ye <ye.na@northeastern.edu>
2021-09-26 00:19:02 -0700

    Update doGet() in SkierServlet

--- server/src/main/java/servlet/SkierServlet.java
+++ server/src/main/java/servlet/SkierServlet.java
@@ -1,5 +1,9 @@
 package servlet;
 
+import com.google.gson.Gson;
+import model.LiftRide;
+import model.SkierVertical;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -7,9 +11,10 @@ import java.io.IOException;
 
 @WebServlet(name = "SkierServlet")
 public class SkierServlet extends HttpServlet {
+    private Gson gson  = new Gson();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/plain");
+        response.setContentType("application/json");
         String urlPath = request.getPathInfo();
 
         // check we have a URL!
@@ -27,9 +32,19 @@ public class SkierServlet extends HttpServlet {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } else {
             response.setStatus(HttpServletResponse.SC_OK);
-            // do any sophisticated processing with urlParts which contains all the url params
-            // TODO: process url params in `urlParts`
-            response.getWriter().write("It works!");
+
+            if (urlParts[2].equals("vertical")) {
+                SkierVertical skierVertical = new SkierVertical(Integer.parseInt(urlParts[1]), -1, "Spring", 99);
+                response.getWriter().write(gson.toJson(skierVertical));
+            } else {
+                response.getWriter().write(gson.toJson(new LiftRide(
+                        Integer.parseInt(urlParts[7]),
+                        -1,
+                        Integer.parseInt(urlParts[1]),
+                        urlParts[4],
+                        urlParts[6],
+                        -1)));
+            }
         }
     }
 
