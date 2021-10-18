3eb58b5b9d9332214e8fa079d36894d37b59fd71
Nan Ye <ye.na@northeastern.edu>
2021-10-17 17:28:45 -0700

    Push messages to RMQ in SkierServlet

--- serverRMQ/src/main/java/servlet/SkierServlet.java
+++ serverRMQ/src/main/java/servlet/SkierServlet.java
@@ -10,10 +10,32 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 @WebServlet(name = "SkierServlet")
 public class SkierServlet extends HttpServlet {
-    private final Gson gson  = new Gson();
+    private static final String POST_LIFT_QUEUE_NAME = "postLiftQ";
+    private static final String HOST_NAME = "localhost";
+    private static Connection conn;
+    private static Gson gson ;
+
+    @Override
+    public void init() {
+        gson = new Gson();
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOST_NAME);
+        try {
+            conn = factory.newConnection();
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
@@ -71,10 +93,31 @@ public class SkierServlet extends HttpServlet {
             response.getWriter().write(gson.toJson(message));
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         } else {
-            LiftEvent lift = gson.fromJson(request.getReader(), LiftEvent.class);
-            response.getWriter().write((gson.toJson(lift)));
-            response.setStatus(HttpServletResponse.SC_CREATED);
+            try {
+                Channel channel = conn.createChannel();
+                channel.queueDeclare(POST_LIFT_QUEUE_NAME, false, false, false, null);
+                LiftEvent lift = gson.fromJson(request.getReader(), LiftEvent.class);
+
+                // message = "skierid,timestamp,liftid"
+                String message = urlParts[7] + "," + lift.getTime() + "," + lift.getLiftId();
+                channel.basicPublish("", POST_LIFT_QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+                channel.close();
+                response.getWriter().write((gson.toJson(lift)));
+                response.setStatus(HttpServletResponse.SC_CREATED);
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    @Override
+    public void destroy() {
+        if (conn != null)
+            try {
+                conn.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
 
     private boolean isUrlValid(String[] urlPath) {
