caef9a6ce5191bfc0a5ce87f314f6c897f56aca6
Nan Ye <ye.na@northeastern.edu>
2021-11-04 11:04:51 -0700

    Update DAO for skier consumer

---
+++ microservice/skierconsumer/src/main/java/dal/LiftRide.java
@@ -0,0 +1,54 @@
+package dal;
+
+public class LiftRide {
+
+    private int skierId;
+    private int liftId;
+    private int resortId;
+    private String season;
+    private int day;
+    private int time;
+
+    public LiftRide(int skierId, int liftId, int resortId, String season, int day, int time) {
+        this.skierId = skierId;
+        this.liftId = liftId;
+        this.resortId = resortId;
+        this.season = season;
+        this.day = day;
+        this.time = time;
+    }
+
+    public int getSkierId() {
+        return skierId;
+    }
+
+    public int getLiftId() {
+        return liftId;
+    }
+
+    public int getResortId() {
+        return resortId;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "skierId=" + skierId +
+                ",liftId=" + liftId +
+                ",resortId=" + resortId +
+                ",season=" + season +
+                ",day=" + day +
+                ",time=" + time;
+    }
+}
--- microservice/skierconsumer/src/main/java/dal/LiftRideDao.java
+++ microservice/skierconsumer/src/main/java/dal/LiftRideDao.java
@@ -2,7 +2,6 @@ package dal;
 
 import java.sql.*;
 
-import model.LiftRide;
 import org.apache.commons.dbcp2.*;
 
 public class LiftRideDao {
@@ -15,7 +14,7 @@ public class LiftRideDao {
     public void createLiftRide(LiftRide newLiftRide) {
         Connection conn = null;
         PreparedStatement preparedStatement = null;
-        String insertQueryStatement = "INSERT INTO LiftRides (skierId, liftId, resortId, seasonId, dayId, time) " +
+        String insertQueryStatement = "INSERT INTO LiftRides (skierId, liftId, resortId, seasonId, day, time) " +
                 "VALUES (?,?,?,?,?,?)";
         try {
             conn = dataSource.getConnection();
@@ -23,8 +22,8 @@ public class LiftRideDao {
             preparedStatement.setInt(1, newLiftRide.getSkierId());
             preparedStatement.setInt(2, newLiftRide.getLiftId());
             preparedStatement.setInt(3, newLiftRide.getResortId());
-            preparedStatement.setString(4, newLiftRide.getSeasonId());
-            preparedStatement.setString(5, newLiftRide.getDayId());
+            preparedStatement.setString(4, newLiftRide.getSeason());
+            preparedStatement.setInt(5, newLiftRide.getDay());
             preparedStatement.setInt(6, newLiftRide.getTime());
 
             // execute insert SQL statement
